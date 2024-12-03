01f501401922b298b9c632a59db2008e0532ff5a
Matias P. <matias.penailillob@gmail.com>
2024-12-02 22:45:36 -0300

    refactor: add exception

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -48,7 +49,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     })
 
     if(!product) {
-      throw new NotFoundException(`Product with id ${id} not found`)
+      throw new RpcException({status: HttpStatus.BAD_REQUEST,  message:`Product with id ${id} not found`})
     }
 
     return product;
